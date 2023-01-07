d031e882f8399036e55cc6fbfe2d05a27e76cfde
Furqan hasan <furqan.hasan27@gmail.com>
2023-01-07 23:21:50 +0530

    changes

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FeaturedCourses from './FeaturedCourses'
+import HeroSection from './HeroSection'
+
+function Home() {
+  return (
+    <div>
+        <HeroSection/>
+        <FeaturedCourses/>
+    </div>
+  )
+}
+
+export default Home
