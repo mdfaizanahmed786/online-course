dc96fc550fdd1877e186f2c0b123455e18aedbac
Furqan hasan <furqan.hasan27@gmail.com>
2023-01-07 22:30:46 +0530

    added dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Course from './components/Course/Course';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Course/>
+    <Login/>
     <Footer/>
     </div>
   );
