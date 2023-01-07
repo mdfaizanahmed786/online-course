d9538d06de15a5c4ba2e99e72f6ad4d081df31d9
faizan <faizanzaid789@gmail.com>
2023-01-08 01:11:18 +0530

    added few pages.

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,25 +1,85 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Signup() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [check, setCheck] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    localStorage.setItem("check", check);
+  };
+
   return (
-    <div style={{height:'100vh', paddingTop:'120px', maxWidth:"800px", margin:'auto'}}><form>
-       <h2 className='mb-5'>Signup to get started!</h2>
-    <div className="mb-3">
-      <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-      <div id="emailHelp" className="form-text">We'll never share your email with anyone else .</div>
+    <div
+      style={{
+        height: "100vh",
+        paddingTop: "120px",
+        maxWidth: "800px",
+        margin: "auto",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <h2 className="mb-5">Signup to get started!</h2>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={handleChange}
+            value={user.email}
+            name="email"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={handleChange}
+            value={user.password}
+            name="password"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+            onChange={(e) => setCheck(!e)}
+            checked={check}
+            name="check"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
-    <div className="mb-3">
-      <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-      <input type="password" className="form-control" id="exampleInputPassword1"/>
-    </div>
-    <div className="mb-3 form-check">
-      <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-      <label className="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </form></div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
