c7dc34e5bca9ff11a2fd4181120046532f6cfd7e
Furqan hasan <furqan.hasan27@gmail.com>
2023-01-07 22:55:06 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Course from './components/Course/Course';
 import Login from './components/Login';
-
+import Signup from './components/Signup';
+import {Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
     <Navbar/>
-    <Course/>
-    <Login/>
+    <Routes >
+    <Route element={<Course/>} path="/course" />
+    <Route element={<Login/>} path="/login" />
+    <Route element={<Signup/>} path="/signup" />
+    </Routes>
     <Footer/>
     </div>
   );
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOlBcYwF_lHPJ-VmeKGIM3y_bPo0T4CFc",
+  authDomain: "educational-courses-b1a69.firebaseapp.com",
+  projectId: "educational-courses-b1a69",
+  storageBucket: "educational-courses-b1a69.appspot.com",
+  messagingSenderId: "1072839883881",
+  appId: "1:1072839883881:web:5a7585a2a750292403f6f8"
+};
+
+
+const app = initializeApp(firebaseConfig);
