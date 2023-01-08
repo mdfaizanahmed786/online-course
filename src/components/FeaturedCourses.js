6e472fb3d91b20d300e2bf0ea645d9bb124c68a0
faizan <faizanzaid789@gmail.com>
2023-01-08 11:24:27 +0530

    added this course page

--- src/components/FeaturedCourses.js
+++ src/components/FeaturedCourses.js
@@ -1,14 +1,29 @@
-import React from 'react'
-import Card from './Card'
-function FeaturedCourses({courses}) {
+import React from "react";
+import Card from "./Card";
+function FeaturedCourses({ courses }) {
   return (
-    <div style={{display:'grid', gridTemplateColumns:'1fr 1fr 1fr', gap:'10px'}}>{
-        courses.slice(0,3).map((course,index)=>{
-          return <Card key={index} title={course.courseName} description={course.courseDescription} image={course.courseImage}/>
-          }) 
-   
-       }</div>
-  )
+    <div>
+      <h2 className="my-lg-5">Featured Courses</h2>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          gap: "10px",
+        }}
+      >
+        {courses.slice(0, 3).map((course, index) => {
+          return (
+            <Card
+              key={index}
+              title={course.courseName}
+              description={course.courseDescription}
+              image={course.courseImage}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default FeaturedCourses
+export default FeaturedCourses;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 function HeroSection() {
   return (
     <div className="d-flex justify-content-between align-items-center">
       <div>
-        <h1>Learn Anything</h1>
+        <TypeAnimation
+          sequence={[
+          "Learn",
+          300,
+          "Anything!",
+          300,
+
+          "Learn Anything!",
+          300,]}
+          wrapper="h1"
+          repeat={Infinity}
+          speed={30}
+        
+        />
         <p>Learn anything from anywhere</p>
       </div>
       <div>
         <img
           src="https://cdn.pixabay.com/photo/2019/11/04/10/15/book-4600757_960_720.jpg"
           className="object-fit-contain rounded-5"
-          style={{height: "500px", width: "500px", borderRadius: "10px"}}
+          style={{ height: "500px", width: "500px", borderRadius: "70px" }}
           alt="learn_image"
         />
       </div>
