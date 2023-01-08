e823484d7a076cf4131b621dec3cdfe7eb9f793a
faizan <faizanzaid789@gmail.com>
2023-01-08 10:43:07 +0530

    added loading spinner

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-function Card({title, description, image}) {
+function Card({ title, description, image }) {
   return (
     <div>
       <div className="card" style={{ width: "18rem" }}>
         <img src={image} className="card-img-top" alt="course_image" />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text">
-           {description.slice(0,60)}
-          </p>
-          <a href="/" className="btn btn-primary">
-            Go somewhere
+          <p className="card-text">{description.slice(0, 60)}</p>
+          <a href="/" className="btn btn-primary me-3">
+            Enroll Now
+          </a>
+          <a href="/" className="btn btn-danger ">
+            Start Quiz
           </a>
         </div>
       </div>
--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card";
+import { Vortex } from "react-loader-spinner";
 
 function Course({ setProgress }) {
-  const [courses, setCourses]=useState([]);
+  const [courses, setCourses] = useState([]);
+  const [loader, setLoader] = useState(false);
   useEffect(() => {
     setProgress(60);
     setTimeout(() => {
       setProgress(100);
     }, 300);
-     // eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
-const fetchData=async ()=>{
-  const data=await fetch("https://courses-backend-2rjr.onrender.com/");
-  const response=await data.json();
-  setCourses(response);
-}
-  useEffect(()=>{
-     fetchData();
-  },[])
+  const fetchData = async () => {
+    setLoader(true);
+    const data = await fetch("https://courses-backend-2rjr.onrender.com/");
+    setLoader(true);
+    const response = await data.json();
+    setCourses(response);
+    setLoader(false)
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div style={{  maxWidth: "1000px", margin: "auto" }}>
+    <div style={{ maxWidth: "1000px", margin: "auto" }}>
       <h1 style={{ textAlign: "center", marginTop: "50px" }}>Courses</h1>
+      <div className="d-flex  justify-content-center">
+        {loader && (
+          <Vortex
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="vortex-loading"
+            wrapperStyle={{}}
+            wrapperClass="vortex-wrapper"
+            colors={["red", "green", "blue", "yellow", "orange", "purple"]}
+          />
+        )}
+      </div>
       <div
         style={{
           display: "grid",
@@ -30,7 +48,14 @@ const fetchData=async ()=>{
         }}
       >
         {courses.map((course, index) => {
-          return <Card key={index} title={course.courseName} description={course.courseDescription} image={course.courseImage}/>;
+          return (
+            <Card
+              key={index}
+              title={course.courseName}
+              description={course.courseDescription}
+              image={course.courseImage}
+            />
+          );
         })}
       </div>
     </div>
