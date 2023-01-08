bc095d9119ed193a173162d3ef59d7b1d53dec74
faizan <faizanzaid789@gmail.com>
2023-01-08 15:41:03 +0530

    added url links to the main page..

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,11 +8,11 @@ function Card({ title, description, image, user }) {
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description.slice(0, 60)}</p>
-          <a href="/" className="btn btn-primary me-3">
+          <a href="https://www.udemy.com/course/web-development-masterclass-complete-certificate-course/" className="btn btn-primary me-3">
             Enroll Now
           </a>
           <button className="btn btn-danger " disabled={!user}>
-            Start Quiz
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSc1pJI-1L473RbznMcWcyvHEsITOpN_YhEErEZlU4p9_WCDzQ/viewform" style={{textDecoration:"none", color:'white'}}>Start Quiz</a>
           </button>
         </div>
       </div>
